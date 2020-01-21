93f4154f27a3426b9743eb0167fe245534df0084
Steven Haid <stevenhaid@gmail.com>
2020-01-20 19:57:36 -0500

    comment display.c

--- display.c
+++ display.c
@@ -1,6 +1,3 @@
-// XXX
-//#define ENABLE_LOGGING_AT_DEBUG_LEVEL
-
 #include "common.h"
 
 //
@@ -9,6 +6,8 @@
 
 #define DEFAULT_WIN_WIDTH  1920
 #define DEFAULT_WIN_HEIGHT 1080
+
+#define FONTSZ 24
 
 //
 // typedefs
@@ -33,6 +32,7 @@ static int param_values_pane_hndlr(pane_cx_t * pane_cx, int request, void * init
 void display_handler(void)
 {
     int win_width, win_height;
+    int screen_image_pane_width;
 
     // init sdl, and get actual window width and height
     win_width  = DEFAULT_WIN_WIDTH;
@@ -45,16 +45,22 @@ void display_handler(void)
 
     // call the pane manger; 
     // this will not return except when it is time to terminate the program
-int xxx = win_width * .70;
+    screen_image_pane_width = win_width * .70;
     sdl_pane_manager(
         NULL,           // context
         NULL,           // called prior to pane handlers
         NULL,           // called after pane handlers
         100000,         // 0=continuous, -1=never, else us 
         3,              // number of pane handler varargs that follow
-        screen_image_pane_hndlr, NULL, 0, 0, xxx, win_height, PANE_BORDER_STYLE_MINIMAL,
-        param_select_pane_hndlr, NULL, xxx, 0, win_width-xxx, win_height/2, PANE_BORDER_STYLE_MINIMAL,
-        param_values_pane_hndlr, NULL, xxx, win_height/2, win_width-xxx, win_height/2, PANE_BORDER_STYLE_MINIMAL
+        screen_image_pane_hndlr, NULL, 
+            0, 0, screen_image_pane_width, win_height, 
+            PANE_BORDER_STYLE_MINIMAL,
+        param_select_pane_hndlr, NULL, 
+            screen_image_pane_width, 0, win_width-screen_image_pane_width, win_height/2, 
+            PANE_BORDER_STYLE_MINIMAL,
+        param_values_pane_hndlr, NULL, 
+            screen_image_pane_width, win_height/2, win_width-screen_image_pane_width, win_height/2, 
+            PANE_BORDER_STYLE_MINIMAL
         );
 }
 
@@ -66,7 +72,6 @@ static int screen_image_pane_hndlr(pane_cx_t * pane_cx, int request, void * init
         int none;
     } * vars = pane_cx->vars;
     rect_t * pane = &pane_cx->pane;
-    int fontsz = 24;
 
     // ----------------------------
     // -------- INITIALIZE --------
@@ -85,14 +90,17 @@ static int screen_image_pane_hndlr(pane_cx_t * pane_cx, int request, void * init
 
     if (request == PANE_HANDLER_REQ_RENDER) {
         param_t *p = &param[param_select_idx];
-        int ytop, ybottom, xbase, first_graph_idx, last_graph_idx;
+        int ytop, ybottom, xbase, first_graph_idx, last_graph_idx, i;
 
+        // print the selected param status_str
         sdl_render_printf(
-                pane, COL2X(20,fontsz), pane->h-ROW2Y(1,fontsz), fontsz,
+                pane, COL2X(30,FONTSZ), pane->h-ROW2Y(2,FONTSZ), FONTSZ,
                 WHITE, BLACK,
                 "%s",
                 p->status_str);
 
+        // set local variables used to display the screen intensity graph;
+        // this graph is displayed vertically on the right side of the pane
         xbase           = pane->w - 160;
         ytop            = (pane->h - MAX_GRAPH) / 2;
         ybottom         = ytop + MAX_GRAPH - 1;
@@ -106,26 +114,39 @@ static int screen_image_pane_hndlr(pane_cx_t * pane_cx, int request, void * init
             last_graph_idx -= (ybottom - pane->h + 1);
             ybottom = pane->h - 1;
         }
-        //INFO("y-range  %d %d  graph-range  %d %d \n", ytop, ybottom,  first_graph_idx, last_graph_idx);
 
+        // draw vertical reference coordinate line for the screen, and
+        // draw the scale labels on this line
         sdl_render_line(pane, xbase, ytop, xbase, ybottom, WHITE);
 
-        // make array of graph points
+        { int ycenter, n, ylabel;
+        ycenter = (ybottom + ytop) / 2;
+        n = (ycenter - ytop) / 100;
+        ylabel = ycenter - 100 * n;
+        for (i = 0; i <= 2*n; i++) {
+            sdl_render_line(pane, xbase, ylabel, xbase+10, ylabel, WHITE);
+            sdl_render_printf(
+                pane, xbase+20, ylabel-sdl_font_char_height(FONTSZ)/2, FONTSZ,
+                WHITE, BLACK,
+                "%+0.2f mm", 
+                (n-i) * 100 * GRAPH_ELEMENT_SIZE * 1e3);
+            ylabel += 100;
+        } }
+
+        // if the selected param's graph is available then plot it just to the left 
+        // of the vertical reference displayed by the code above
         if (p->graph) {
-            int y;
-            int max_points;
-            int graph_idx;
+            int    y;
+            int    max_points = 0;
+            int    graph_idx = first_graph_idx;
             static point_t points[10000];
 
-            max_points = 0;
-            graph_idx = first_graph_idx;
             for (y = ytop; y <= ybottom; y++) {
-                points[max_points].x = xbase-10 - p->graph[graph_idx] * 700; //XXX AAA
+                points[max_points].x = xbase-10 - p->graph[graph_idx] * 700;
                 points[max_points].y = y;
                 graph_idx++;
                 max_points++;
             }
-            //INFO("max_points %d\n", max_points);
 
             if (graph_idx-1 != last_graph_idx) {
                 FATAL("BUG graph_idx=%d last_graph_idx=%d\n", graph_idx, last_graph_idx);
@@ -134,48 +155,28 @@ static int screen_image_pane_hndlr(pane_cx_t * pane_cx, int request, void * init
             sdl_render_lines(pane, points, max_points, WHITE);
         }
 
-        // XXX test line in ctr
-
-        // label the graph
-        int ctr, n, ylabel, i;
-        ctr = (ybottom + ytop) / 2;
-        //XXX del sdl_render_line(pane, xbase-100, ctr, xbase, ctr, RED);
-        n = (ctr - ytop) / 100;
-        //INFO("ctr=%d  n=%d\n", ctr, n);
-        ylabel = ctr - 100 * n;
-        for (i = 0; i <= 2*n; i++) {
-            sdl_render_line(pane, xbase, ylabel, xbase+10, ylabel, WHITE);
-            sdl_render_printf(
-                pane, xbase+20, ylabel-sdl_font_char_height(fontsz)/2, fontsz,
-                WHITE, BLACK,
-                "%+0.2f mm", 
-                (n-i) * 100 * GRAPH_ELEMENT_SIZE * 1e3);
-            ylabel += 100;
-        }
-
-        // AAAAAAAAAAAAA
-//AAA
+        // draw vertical reference coordinate line for the slit source(s), and
+        // draw the scale labels on this line; this will be the same length
+        // as the vertical reference line drawn above for the screen
         xbase = 140;
         sdl_render_line(pane, xbase, ytop, xbase, ybottom, WHITE);
 
-        // label the graph
-        //int ctr, n, ylabel, i;
-        ctr = (ybottom + ytop) / 2;
-        //XXX del sdl_render_line(pane, xbase-100, ctr, xbase, ctr, RED);
-        n = (ctr - ytop) / 100;
-        //INFO("ctr=%d  n=%d\n", ctr, n);
-        ylabel = ctr - 100 * n;
+        { int ycenter, n, ylabel;
+        ycenter = (ybottom + ytop) / 2;
+        n = (ycenter - ytop) / 100;
+        ylabel = ycenter - 100 * n;
         for (i = 0; i <= 2*n; i++) {
             sdl_render_line(pane, xbase-10, ylabel, xbase, ylabel, WHITE);
             sdl_render_printf(
-                pane, 0, ylabel-sdl_font_char_height(fontsz)/2, fontsz,
+                pane, 0, ylabel-sdl_font_char_height(FONTSZ)/2, FONTSZ,
                 WHITE, BLACK,
                 "%+0.2f mm", 
                 (n-i) * 100 * GRAPH_ELEMENT_SIZE/10 * 1e3);  // XXX define for 10
             ylabel += 100;
-        }
+        } }
 
-
+        // draw the slits by overwriting the slit vertical reference line drawn
+        // above with black lines representing the slits
         for (i = 0; i < p->max_slit; i++) {
             int yslit_start, yslit_end;
             yslit_start =  (ytop + ybottom) / 2 + p->slit[i].start / (GRAPH_ELEMENT_SIZE/10);
@@ -215,7 +216,6 @@ static int param_select_pane_hndlr(pane_cx_t * pane_cx, int request, void * init
     } * vars = pane_cx->vars;
     rect_t * pane = &pane_cx->pane;
     int i;
-    int fontsz = 24;
 
    #define SDL_EVENT_PARAM_SELECT (SDL_EVENT_USER_DEFINED + 0)
 
@@ -235,9 +235,10 @@ static int param_select_pane_hndlr(pane_cx_t * pane_cx, int request, void * init
     // ------------------------
 
     if (request == PANE_HANDLER_REQ_RENDER) {
+        // display names and register events for all params
         for (i = 0; i < max_param; i++) {
             sdl_render_text_and_register_event(
-                pane, COL2X(0,fontsz), ROW2Y(i,fontsz), fontsz,
+                pane, COL2X(0,FONTSZ), ROW2Y(i,FONTSZ), FONTSZ,
                 param[i].name,
                 (i != param_select_idx ? LIGHT_BLUE : WHITE), BLACK,
                 SDL_EVENT_PARAM_SELECT+i,
@@ -254,6 +255,8 @@ static int param_select_pane_hndlr(pane_cx_t * pane_cx, int request, void * init
     if (request == PANE_HANDLER_REQ_EVENT) {
         switch (event->event_id) {
         case SDL_EVENT_PARAM_SELECT...SDL_EVENT_PARAM_SELECT+MAX_PARAM-1:
+            // set the selected param as the active param being displayed, and
+            // start the calculation of the screen image for this selected param
             param_select_idx = event->event_id - SDL_EVENT_PARAM_SELECT;
             calculate_screen_image(&param[param_select_idx]);
             DEBUG("set param_select_idx = %d\n", param_select_idx);
@@ -282,7 +285,6 @@ static int param_values_pane_hndlr(pane_cx_t * pane_cx, int request, void * init
         int none;
     } * vars = pane_cx->vars;
     rect_t * pane = &pane_cx->pane;
-    int fontsz = 24;  // XXX define
 
     // ----------------------------
     // -------- INITIALIZE --------
@@ -303,23 +305,28 @@ static int param_values_pane_hndlr(pane_cx_t * pane_cx, int request, void * init
         param_t *p = &param[param_select_idx];
         int i;
 
+        // print the selected param values:
+        // - name
+        // - distance to screen
+        // - wavelength
+        // - slit info table
         sdl_render_printf(
-                pane, COL2X(0,fontsz), ROW2Y(0,fontsz), fontsz,
+                pane, COL2X(0,FONTSZ), ROW2Y(0,FONTSZ), FONTSZ,
                 WHITE, BLACK,
                 "%s", 
                 p->name);
         sdl_render_printf(
-                pane, COL2X(0,fontsz), ROW2Y(2,fontsz), fontsz,
+                pane, COL2X(0,FONTSZ), ROW2Y(2,FONTSZ), FONTSZ,
                 WHITE, BLACK,
                 "distance   = %.3f m", 
                 p->distance_to_screen);
         sdl_render_printf(
-                pane, COL2X(0,fontsz), ROW2Y(3,fontsz), fontsz,
+                pane, COL2X(0,FONTSZ), ROW2Y(3,FONTSZ), FONTSZ,
                 WHITE, BLACK,
                 "wavelength = %.0f nm", 
                 p->wavelength*1e9);
         sdl_render_printf(
-                pane, COL2X(0,fontsz), ROW2Y(5,fontsz), fontsz,
+                pane, COL2X(0,FONTSZ), ROW2Y(5,FONTSZ), FONTSZ,
                 WHITE, BLACK,
                 " start    end  width center");
         for (i = 0; i < p->max_slit;i++) {
@@ -328,7 +335,7 @@ static int param_values_pane_hndlr(pane_cx_t * pane_cx, int request, void * init
             double width_mm  = (end_mm - start_mm);
             double center_mm = (end_mm + start_mm) / 2;
             sdl_render_printf(
-                    pane, COL2X(0,fontsz), ROW2Y(6+i,fontsz), fontsz,
+                    pane, COL2X(0,FONTSZ), ROW2Y(6+i,FONTSZ), FONTSZ,
                     WHITE, BLACK,
                     "%6.2f %6.2f %6.2f %6.2f",
                     start_mm, end_mm, width_mm, center_mm);
