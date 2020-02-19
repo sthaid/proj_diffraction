99fba774b539d7edddc7f9d1402733d06532ca7e
Steven Haid <stevenhaid@gmail.com>
2020-02-19 08:17:06 -0500

    update photons.c, mainly to account for the fraction of photons passing through the double slit

--- devel/photons/photons.c
+++ devel/photons/photons.c
@@ -13,9 +13,7 @@
 // https://www.edmundoptics.com/knowledge-center/application-notes/optics/understanding-neutral-density-filters/
 //
 
-#define RED    0
-#define GREEN  1
-#define BLUE   2
+#define MAX_COLOR_TBL (sizeof(color_tbl) / sizeof(color_tbl[0]))
 
 struct {
     char *name;
@@ -29,10 +27,11 @@ struct {
 
 const double h                = 6.626e-34;          // planck constant
 const double c                = 2.998e8;            // speed of light
-const double detector_area    = (.1e-3 * 1e-3);     // .1mm x 1mm
-const double pattern_area     = (100e-3 * 10e-3);   // 100mm x 10mm
 const double laser_power      = 5e-3;               // 5 milliwatt output beam power
 const double filter_od        = 7;                  // filter optical density
+const double double_slit_pass = 0.2;                // fraction of laser photons which pass through double slit
+const double pattern_area     = (100e-3 * 10e-3);   // 100mm x 10mm
+const double detector_area    = (.1e-3 * 1e-3);     // .1mm x 1mm
 
 void calculate_detector_count_rate(int color);
 
@@ -40,9 +39,18 @@ void calculate_detector_count_rate(int color);
 
 int main(int argc, char **argv)
 {
-    calculate_detector_count_rate(RED);
-    calculate_detector_count_rate(GREEN);
-    calculate_detector_count_rate(BLUE);
+    int i;
+
+    printf("laser_power        = %.3f watts (joules/sec)\n",   laser_power);
+    printf("filter_od          = %.3f\n",                      filter_od);
+    printf("double_slit_pass   = %.2f percent\n",              double_slit_pass * 100);
+    printf("pattern_area       = %.1f mm^2\n",                 pattern_area * 1e6);
+    printf("detector_area      = %.1f mm^2\n",                 detector_area * 1e6);
+    printf("\n");
+
+    for (i = 0; i < MAX_COLOR_TBL; i++) {
+        calculate_detector_count_rate(i);
+    }
 
     return 0;
 }
@@ -51,17 +59,20 @@ void calculate_detector_count_rate(int color)
 {
     char *name;
     double wavelength, detector_effeciency;
-    double photon_rate_before_filter, photon_rate_after_filter;
+    double photon_rate_at_laser, photon_rate_after_filter, photon_rate_after_double_slit;
     double detector_count_rate, intvl_ns;
 
     name = color_tbl[color].name;
     wavelength = color_tbl[color].wavelength;
     detector_effeciency = color_tbl[color].detector_effeciency;
 
-    photon_rate_before_filter = laser_power * wavelength / (h * c);
-    photon_rate_after_filter = photon_rate_before_filter * pow(10, -filter_od);
-    detector_count_rate = photon_rate_after_filter * (detector_area / pattern_area) * detector_effeciency;
-    intvl_ns = 1 / photon_rate_after_filter * 1e9;
+    photon_rate_at_laser          = laser_power * wavelength / (h * c);
+    photon_rate_after_filter      = photon_rate_at_laser * pow(10, -filter_od);
+    photon_rate_after_double_slit = photon_rate_after_filter * double_slit_pass;
+    detector_count_rate           = photon_rate_after_double_slit * 
+                                   (detector_area / pattern_area) * 
+                                   detector_effeciency;
+    intvl_ns                      = 1 / photon_rate_after_double_slit * 1e9;
 
     // notes:
     // - intvl_ns is the average interval between photons after the filter
@@ -71,6 +82,6 @@ void calculate_detector_count_rate(int color)
           name, 
           detector_count_rate, 
           intvl_ns,
-          photon_rate_before_filter, 
-          photon_rate_after_filter);
+          photon_rate_at_laser, 
+          photon_rate_after_double_slit);
 }
