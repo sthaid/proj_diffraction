5ade22f9c76d1e8e6207173b0c37d0f58b98225a
Steven Haid <stevenhaid@gmail.com>
2020-05-31 08:20:27 -0400

    update dra_blink.c to blink led for testing with the SIPM

--- gpio/dra_blink.c
+++ gpio/dra_blink.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 #include <dra_gpio.h>
 
+#define GPIO_PIN 21
+
 int main(int argc, char **argv)
 {
     if (gpio_init() != 0) {
         return 1;
     }
 
-    set_gpio_pin_mode(18, PIN_MODE_OUTPUT);
+    set_gpio_pin_mode(GPIO_PIN, PIN_MODE_OUTPUT);
 
     while (1) {
-        gpio_write(18, 1);
-        sleep(1);
-        gpio_write(18, 0);
-        sleep(1);
+        printf("TURN ON\n");
+        gpio_write(GPIO_PIN, 1);
+        sleep(10);
+        printf("TURN OFF\n");
+        gpio_write(GPIO_PIN, 0);
+        sleep(10);
     }
         
     return 0;
