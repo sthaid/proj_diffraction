762c5a1a52c76ed4cdc47962217411f7526e47d2
Steven Haid <stevenhaid@gmail.com>
2020-06-16 20:07:42 -0400

    add common_include.h

---
+++ xrpm/common_includes.h
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+
+#include <time.h>
+#include <math.h>
+#include <pthread.h>
+#include <inttypes.h>
+
+#include <sys/resource.h>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
